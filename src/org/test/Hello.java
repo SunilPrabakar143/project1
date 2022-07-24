e8f45e323b2f751d4c37d424ee22851bab7e7f9b
SunilPrabakar143 <sprupgraded@gmail.com>
2022-07-24 09:27:11 +0530

    new code

---
+++ src/org/test/Hello.java
@@ -0,0 +1,16 @@
+package org.test;
+
+public class Hello {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 100; i++) {
+			if (i == 5) {
+			}
+			System.out.println(i);
+			System.out.println(i);
+			System.out.println(i);
+
+			
+		}
+
+	}
+}
---
+++ src/org/test/Test.java
@@ -0,0 +1,13 @@
+package org.test;
+
+public class Test {
+	public static void main(String[] args) {
+		int age = 2, height = 500;
+		if (age < 5 || height < 500) {
+			System.out.println("Half ticket");
+		} else {
+			System.out.println("Full ticket");
+		}
+	}
+
+}
